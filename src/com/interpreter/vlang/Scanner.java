9b7bfd56ce7e7862d99a4436d7a0b9eab9c0af99
Vinitvh <vineet.vh@gmail.com>
2025-03-30 22:02:59 +0530

    Add Initial structural classes

---
+++ src/com/interpreter/vlang/Scanner.java
@@ -0,0 +1,58 @@
+package com.interpreter.vlang;
+
+import static com.interpreter.vlang.TokenType.*;
+import java.util.ArrayList;
+import java.util.List;
+
+class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+
+    Scanner(String source) {
+        this.source = source;
+    }
+
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            start = current;
+            scanToken();
+        }
+
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(':
+                addToken(LEFT_PAREN);
+                break;
+            default:
+                Vlang.error(line, "Unexpected character.");
+                break;
+        }
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+
+    private char advance() {
+        current++;
+        return source.charAt(current - 1);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
+}
